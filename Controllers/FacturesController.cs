6c4c4eb568d8140d96ad488cfd3ebed800050975
Marc <mnoris.xeno@gmail.cm>
2020-02-13 10:08:02 +0100

    Suppression WeatherForecast ; Ajout des controllers Redéfinition URL démarrage

---
+++ Controllers/FacturesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GrandHotel.Models;
+
+namespace GrandHotel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturesController : ControllerBase
+    {
+        private readonly GrandHotelContext _context;
+
+        public FacturesController(GrandHotelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Factures
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Facture>>> GetFacture()
+        {
+            return await _context.Facture.ToListAsync();
+        }
+
+        // GET: api/Factures/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Facture>> GetFacture(int id)
+        {
+            var facture = await _context.Facture.FindAsync(id);
+
+            if (facture == null)
+            {
+                return NotFound();
+            }
+
+            return facture;
+        }
+
+        // PUT: api/Factures/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFacture(int id, Facture facture)
+        {
+            if (id != facture.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(facture).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FactureExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Factures
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Facture>> PostFacture(Facture facture)
+        {
+            _context.Facture.Add(facture);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFacture", new { id = facture.Id }, facture);
+        }
+
+        // DELETE: api/Factures/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Facture>> DeleteFacture(int id)
+        {
+            var facture = await _context.Facture.FindAsync(id);
+            if (facture == null)
+            {
+                return NotFound();
+            }
+
+            _context.Facture.Remove(facture);
+            await _context.SaveChangesAsync();
+
+            return facture;
+        }
+
+        private bool FactureExists(int id)
+        {
+            return _context.Facture.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GrandHotel.Models;
+
+namespace GrandHotel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly GrandHotelContext _context;
+
+        public ReservationsController(GrandHotelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reservations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetReservation()
+        {
+            return await _context.Reservation.ToListAsync();
+        }
+
+        // GET: api/Reservations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservation>> GetReservation(short id)
+        {
+            var reservation = await _context.Reservation.FindAsync(id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return reservation;
+        }
+
+        // PUT: api/Reservations/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReservation(short id, Reservation reservation)
+        {
+            if (id != reservation.NumChambre)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reservation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reservations
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Reservation>> PostReservation(Reservation reservation)
+        {
+            _context.Reservation.Add(reservation);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReservationExists(reservation.NumChambre))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetReservation", new { id = reservation.NumChambre }, reservation);
+        }
+
+        // DELETE: api/Reservations/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Reservation>> DeleteReservation(short id)
+        {
+            var reservation = await _context.Reservation.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservation.Remove(reservation);
+            await _context.SaveChangesAsync();
+
+            return reservation;
+        }
+
+        private bool ReservationExists(short id)
+        {
+            return _context.Reservation.Any(e => e.NumChambre == id);
+        }
+    }
+}
