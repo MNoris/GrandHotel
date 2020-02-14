80b70bb584e02b18126cbd80311a057f40938f0d
Marc <mnoris.xeno@gmail.cm>
2020-02-14 15:53:13 +0100

    Fin ReservationsController Ajout du .json

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using GrandHotel.Models;
+using Microsoft.Data.SqlClient;
 
 namespace GrandHotel.Controllers
 {
@@ -24,6 +25,7 @@ namespace GrandHotel.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Reservation>>> GetReservation([FromQuery]DateTime date)
         {
+            //Vérifie que la date a bien été passée en paramètre
             if (date == DateTime.MinValue)
                 return BadRequest("Aucune date n'a été passée en paramètre");
 
@@ -64,33 +66,32 @@ namespace GrandHotel.Controllers
         // PUT: api/Reservations/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutReservation(short id, Reservation reservation)
+        [HttpPut]
+        public async Task<IActionResult> PutReservation(Reservation reservation)
         {
-            if (id != reservation.NumChambre)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(reservation).State = EntityState.Modified;
-
             try
             {
+                if (_context.Reservation.Find(reservation.NumChambre, reservation.Jour).IdClient != reservation.IdClient)
+                    return Conflict("Réservation déjà existante pour un client différent.");
+
+                //On détache l'entité déjà sauvegardée en local, 
+                var local = _context.Reservation.Local.FirstOrDefault(r => r.NumChambre == reservation.NumChambre & r.Jour == reservation.Jour);
+                _context.Entry(local).State = EntityState.Detached;
+                
+                _context.Entry(reservation).State = EntityState.Modified;
+
                 await _context.SaveChangesAsync();
+                await _context.Entry(reservation).ReloadAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception)
             {
-                if (!ReservationExists(id))
-                {
-                    return NotFound();
-                }
+                if (!ReservationExists(reservation.NumChambre, reservation.Jour))
+                    return NotFound("Aucune réservation de cette chambre pour ce jour la.");
                 else
-                {
-                    throw;
-                }
+                    return BadRequest();
             }
 
-            return NoContent();
+            return Ok(reservation);
         }
 
         // POST: api/Reservations
@@ -99,34 +100,35 @@ namespace GrandHotel.Controllers
         [HttpPost]
         public async Task<ActionResult<Reservation>> PostReservation(Reservation reservation)
         {
-            _context.Reservation.Add(reservation);
             try
             {
+                //Si la clé numChambre + jour existe déjà, renvoie un conflit
+                if (ReservationExists(reservation.NumChambre, reservation.Jour))
+                    return Conflict("Réservation déjà existante ce jour pour cette chambre.");
+
+                _context.Reservation.Add(reservation);
+
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateException)
+            catch (Exception e)
             {
-                if (ReservationExists(reservation.NumChambre))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
+                SqlException sqle = (SqlException)e.InnerException;
+                if (sqle.Number == 547)
+                    return BadRequest("Cette date n'existe pas dans le calendrier");
+                return BadRequest();
             }
 
-            return CreatedAtAction("GetReservation", new { id = reservation.NumChambre }, reservation);
+            return CreatedAtAction("PostReservation", reservation);
         }
 
-        // DELETE: api/Reservations/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Reservation>> DeleteReservation(short id)
+        // DELETE: api/Reservations/?num=1&date=01/01/2016
+        [HttpDelete]
+        public async Task<ActionResult<Reservation>> DeleteReservation([FromQuery]short num, [FromQuery]DateTime date)
         {
-            var reservation = await _context.Reservation.FindAsync(id);
+            var reservation = await _context.Reservation.FindAsync(num, date);
             if (reservation == null)
             {
-                return NotFound();
+                return NotFound("Réservation inconnue ce jour pour cette chambre");
             }
 
             _context.Reservation.Remove(reservation);
@@ -135,9 +137,9 @@ namespace GrandHotel.Controllers
             return reservation;
         }
 
-        private bool ReservationExists(short id)
+        private bool ReservationExists(short id, DateTime jour)
         {
-            return _context.Reservation.Any(e => e.NumChambre == id);
+            return _context.Reservation.Any(e => e.NumChambre == id && e.Jour == jour);
         }
     }
 }
