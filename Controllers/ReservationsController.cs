029f612cf32b6bf761ae4d1517648f05772f43d9
Marc <mnoris.xeno@gmail.cm>
2020-02-14 12:35:42 +0100

    Maj return & commentaires controllers

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -24,6 +24,9 @@ namespace GrandHotel.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Reservation>>> GetReservation([FromQuery]DateTime date)
         {
+            if (date == DateTime.MinValue)
+                return BadRequest("Aucune date n'a été passée en paramètre");
+
             try
             {
                 var reservations = await _context.Reservation.Where(r => r.Jour.Date == date.Date).ToListAsync();
@@ -36,7 +39,6 @@ namespace GrandHotel.Controllers
             catch (Exception)
             {
                 return BadRequest();
-                throw;
             }
         }
 
@@ -44,24 +46,19 @@ namespace GrandHotel.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<List<Reservation>>> GetReservationsClient(int id)
         {
-
             try
             {
                 var reservation = await _context.Reservation.Where(r => r.IdClient == id).ToListAsync();
 
                 if (reservation.Count() == 0)
-                {
                     return NotFound("Aucune réservations n'ont été trouvées pour ce client.");
-                }
 
                 return reservation;
             }
             catch (Exception)
             {
                 return BadRequest();
-                throw;
             }
-
         }
 
         // PUT: api/Reservations/5
